efa44fd4238e613f10788735b2a2f1e7135648d9
ksandrsl <ksandr.sl@gmail.com>
2023-04-21 16:42:57 +0300

    fix(basic-3.2): meaningful class names

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 // Event handling, user interaction is what starts the code execution.
 
 var taskInput=document.getElementById("new-task");//Add a new task.
-var addButton=document.getElementsByTagName("button")[0];//first button
+var addButton=document.getElementById("add");//add button
 var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
